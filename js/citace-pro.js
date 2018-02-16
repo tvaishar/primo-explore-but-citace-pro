d426c0d89a5f450258ba0f76d46f67b2919c3227
Vaishar Antonín <vaishar@lib.vutbr.cz>
2018-02-16 13:13:51 +0100

    simple controller

--- js/citace-pro.js
+++ js/citace-pro.js
@@ -3,8 +3,28 @@
 *
 */
 
+app.controller ('prmCitationAfterController', ['angularLoad', function (angularLoad) {
+	
+	var cont = this;
+	
+	cont.citacelink = '';
+	
+	cont.author=cont.parentCtlr.item.pnx.addata.au[0];
+	cont.title=cont.parentCtlr.item.pnx.addata.btitle[0];
+
+
+}]);
+
+
+
 app.component('prmCitationAfter', {
-		bindings: { },
-        template: `<div class="citace-pro"><span>Zde bude iframe citace PRO.</span></div>`
+		bindings: {parentCtrl: '<'},
+		controller: 'prmCitationAfterController',
+        template: `<div class="citace-pro"><span>Zde bude iframe citace PRO. Zatím náhled dat:</span>
+		<ul>
+			<li>{{$ctrl.author}}</li>
+			<li>{{$ctrl.title}}</li>
+		</ul>
+		</div>`
  });
 
